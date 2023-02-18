50e9916dfba5a39c9b2341b808d5109ec7400574
k2tzumi <katzumi+github@gmail.com>
2023-02-19 00:46:19 +0900

    ✨ Append openai client

---
+++ src/OpenAiClient.ts
@@ -0,0 +1,158 @@
+import { NetworkAccessError } from "./NetworkAccessError";
+
+type URLFetchRequestOptions = GoogleAppsScript.URL_Fetch.URLFetchRequestOptions;
+type HttpMethod = GoogleAppsScript.URL_Fetch.HttpMethod;
+type HTTPResponse = GoogleAppsScript.URL_Fetch.HTTPResponse;
+
+interface Models {
+  data: {
+    id: string;
+    object: string;
+    owned_by: string;
+    permission: string[];
+  }[];
+}
+
+interface CompletionsResponse {
+  id: string;
+  object: string;
+  created: number;
+  model: string;
+  choices: {
+    text: string;
+    index: number;
+    logprobs: number|null;
+    finish_reason: string;
+  }[];
+  usage: {
+    prompt_tokens: number;
+    completion_tokens: number;
+    total_tokens: number;
+  };
+}
+
+class OpenAiClient {
+  static readonly BASE_PATH = "https://api.openai.com/v1/";
+
+  public constructor(private apiKey: string) {}
+
+  public listModels(): Models {
+    const endPoint = OpenAiClient.BASE_PATH + "models";
+    const payload: {} = {
+    };
+
+    const response: Models = this.invokeAPI(endPoint, payload);
+
+    return response;
+  }
+
+  public completions(prompt: string): CompletionsResponse {
+    const endPoint = OpenAiClient.BASE_PATH + "completions";
+    const payload: {} = {
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 1000
+    };
+
+    const response: CompletionsResponse = this.invokeAPI(endPoint, payload);
+
+    return response;
+  }
+
+  private postRequestHeader() {
+    return {
+      "content-type": "application/json; charset=UTF-8",
+      Authorization: `Bearer ${this.apiKey}`,
+    };
+  }
+
+  private getRequestHeader() {
+    return {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization: `Bearer ${this.apiKey}`,
+    };
+  }
+
+  private postRequestOptions(payload: string | {}): URLFetchRequestOptions {
+    const options: URLFetchRequestOptions = {
+      method: "post",
+      headers: this.postRequestHeader(),
+      muteHttpExceptions: true,
+      payload: payload instanceof String ? payload : JSON.stringify(payload),
+    };
+
+    return options;
+  }
+
+  private getRequestOptions(): URLFetchRequestOptions {
+    const options: URLFetchRequestOptions = {
+      method: "get",
+      headers: this.getRequestHeader(),
+      muteHttpExceptions: true,
+    };
+
+    return options;
+  }
+
+  /**
+   * @param endPoint OpenAI API endpoint
+   * @param options
+   * @throws NetworkAccessError
+   */
+  private invokeAPI(endPoint: string, payload: {}): any {
+    let response: HTTPResponse;
+
+    try {
+      switch (this.preferredHttpMethod(endPoint)) {
+        case "post":
+          response = UrlFetchApp.fetch(
+            endPoint,
+            this.postRequestOptions(payload)
+          );
+          break;
+        case "get":
+          response = UrlFetchApp.fetch(
+            this.formUrlEncoded(endPoint, payload),
+            this.getRequestOptions()
+          );
+          break;
+      }
+    } catch (e) {
+      console.warn(`DNS error, etc. ${e.message}`);
+      throw new NetworkAccessError(500, e.message);
+    }
+
+    switch (response.getResponseCode()) {
+      case 200:
+        return JSON.parse(response.getContentText());
+      default:
+        console.warn(
+          `OpenAI API error. endpoint: ${endPoint}, status: ${response.getResponseCode()}, content: ${response.getContentText()}`
+        );
+        throw new NetworkAccessError(
+          response.getResponseCode(),
+          response.getContentText()
+        );
+    }
+  }
+
+  private preferredHttpMethod(endPoint: string): HttpMethod {
+    switch (true) {
+      case /(.)*models$/.test(endPoint):
+        return "get";
+      default:
+        return "post";
+    }
+  }
+
+  private formUrlEncoded(endPoint: string, payload: {}): string {
+    const query = Object.entries<string>(payload)
+      .map(([key, value]) => `${key}=${encodeURI(value)}`)
+      .join("&");
+
+    return `${endPoint}?${query}`;
+  }
+}
+
+export { OpenAiClient };
