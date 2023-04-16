f3feac6abbe06c18b34522f134dde36577bf2377
Katsumi Kato <k2tzumi@users.noreply.github.com>
2023-04-16 06:54:07 +0000

    Append chat/completions test

--- __tests__/OpenAiClient.spec.ts
+++ __tests__/OpenAiClient.spec.ts
@@ -1,4 +1,4 @@
-import { OpenAiClient } from "../src/OpenAiClient";
+import { Message, OpenAiClient, RoleType } from "../src/OpenAiClient";
 
 console.warn = jest.fn();
 
@@ -45,5 +45,43 @@ describe('OpenAiClient', () => {
             expect(mockFetch.mock.calls[0][1]).toHaveProperty("payload",  "{\"model\":\"text-davinci-003\",\"prompt\":\"prompt\",\"temperature\":0.5,\"top_p\":0.5,\"frequency_penalty\":0.9,\"max_tokens\":1000}");
             expect(actual).toStrictEqual(response);
         });
+    });
+    describe('chatCompletions', () => {
+      it('success', () => {
+        const client = new OpenAiClient('token');
+        response = {
+          "id": "chatcmpl-123",
+          "object": "chat.completion",
+          "created": 1677652288,
+          "choices": [{
+            "index": 0,
+            "message": {
+              "role": "assistant",
+              "content": "\n\nHello there, how may I assist you today?",
+            },
+            "finish_reason": "stop"
+          }],
+          "usage": {
+            "prompt_tokens": 9,
+            "completion_tokens": 12,
+            "total_tokens": 21
+          }
+        }
+        ;
+        const messages = [
+          {
+            role: RoleType.Assistant,
+            content: 'prompt'
+          },
+          {
+            role: RoleType.User,
+            content: 'question'
+          }
+        ] as Message[];
+        const actual = client.chatCompletions(messages);
+        expect(mockFetch.mock.calls[1][0]).toContain('chat/completions');
+        expect(mockFetch.mock.calls[1][1]).toHaveProperty("payload",  "{\"model\":\"gpt-3.5-turbo\",\"messages\":[{\"role\":\"assistant\",\"content\":\"prompt\"},{\"role\":\"user\",\"content\":\"question\"}],\"temperature\":0.5,\"top_p\":0.5,\"frequency_penalty\":0.9,\"max_tokens\":1000}");
+        expect(actual).toStrictEqual(response);
+      });
     });
 });
